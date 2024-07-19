2a0ec08e73127e241b853d87cb1fe154fba21f47
sm4640 <s1m0611@naver.com>
2024-07-19 22:18:05 +0900

    update setting

--- config/urls.py
+++ config/urls.py
@@ -22,11 +22,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
-    path('api/bloodsugars/', include('bloodsugars.urls')),
-    path('api/diets/', include('diets.urls')),
-    path('api/maps/', include('maps.urls')),
-    path('api/podos/', include('podos.urls')),
+    # path('api/accounts/', include('accounts.urls')),
+    # path('api/bloodsugars/', include('bloodsugars.urls')),
+    # path('api/diets/', include('diets.urls')),
+    # path('api/maps/', include('maps.urls')),
+    # path('api/podos/', include('podos.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # MEDIA_URL로 들어오는 요청에 대해 MEDIA_ROOT 경로를 탐색한다.
