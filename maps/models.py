67a8b83e67da17c82f39beb5478081e86461157c
sm4640 <s1m0611@naver.com>
2024-07-21 20:58:41 +0900

    [#5] map 모델 수정

--- maps/models.py
+++ maps/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Map(models.Model):
+    map_id = models.BigAutoField(primary_key=True)
     latitude = models.DecimalField(max_digits=23, decimal_places=20)
     longitude = models.DecimalField(max_digits=24, decimal_places=20)
     place_type = models.CharField(max_length=10)
