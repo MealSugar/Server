6b56a364e1212697dc7604c493d76ef957ce3cd1
sm4640 <s1m0611@naver.com>
2024-07-21 21:47:03 +0900

    [#5] map 모델 mysql 스키마 수정

--- maps/models.py
+++ maps/models.py
@@ -5,4 +5,4 @@ class Map(models.Model):
     map_id = models.BigAutoField(primary_key=True)
     latitude = models.DecimalField(max_digits=23, decimal_places=20)
     longitude = models.DecimalField(max_digits=24, decimal_places=20)
-    place_type = models.CharField(max_length=10)
+    place_type = models.CharField(max_length=15)
