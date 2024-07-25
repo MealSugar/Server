f7c76c94d1a9c47edc8c3156a8fd3903570f215f
largerejade <lsj31200@naver.com>
2024-07-25 16:38:57 +0900

    [#48] Map 모델수정

--- maps/models.py
+++ maps/models.py
@@ -6,3 +6,8 @@ class Map(models.Model):
     latitude = models.DecimalField(max_digits=23, decimal_places=20)
     longitude = models.DecimalField(max_digits=24, decimal_places=20)
     place_type = models.CharField(max_length=15)
+    place_name = models.CharField(max_length=20)
+    place_link = models.URLField(max_length=200)
+    road_name_address = models.CharField(max_length=255)
+    lot_number_address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
