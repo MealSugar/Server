087acd6981f6cd8106172a7aa33db4a739b999e6
sm4640 <s1m0611@naver.com>
2024-08-03 16:47:05 +0900

    static 변경

--- config/settings.py
+++ config/settings.py
@@ -138,13 +138,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    DEBUG = False
-else:
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static',
-    ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 MEDIA_URL = '/media/'
